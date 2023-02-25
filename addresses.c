57dd9a23ece63811746b1197eea62367087b3ca6
Ivan Leskov <ivan.leskov@protonmail.com>
2023-02-25 19:40:09 +0100

    add addresses

--- addresses.c
+++ addresses.c
@@ -13,9 +13,17 @@ int main(void)
     char *s = "HI!";
     printf("%p\n", s);
     printf("%s\n", s);
+    printf("%s\n", s+1);
+    printf("%s\n", s+2);
     printf("%c\n", *s);
+    printf("%c\n", *(s+1));
+    printf("%c\n", *(s+2));
+    // access everything you want in memory
+    printf("%c\n", *(s+5000));
     printf("%p\n", &s[0]);
     printf("%p\n", &s[1]);
     printf("%p\n", &s[2]);
     printf("%p\n", &s[3]);
+    // access everything you want in memory
+    printf("%p\n", &s[5000]);
 }

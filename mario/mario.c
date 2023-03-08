26f88ab4490bae72137b721a483f6603a76a10c7
Ivan Leskov <ivan.leskov@protonmail.com>
2023-03-08 23:24:16 +0100

    add mario advanced

---
+++ mario/mario.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+
+int main() {
+    int height;
+    do
+    {
+        printf("Height: ");
+        scanf("%d", &height);
+    }
+    while(height < 1 || height > 8);
+
+
+    for (int i = 1; i <= height; i++) {
+        for (int j = 1; j <= height - i; j++) {
+            printf(" ");
+        }
+        for (int k = 1; k <= i; k++) {
+            printf("#");
+        }
+        printf("   ");
+        for (int k = 1; k <= i; k++) {
+            printf("#");
+        }
+        for (int j = 1; j <= height - i; j++) {
+            printf(" ");
+        }
+        printf("\n");
+    }
+
+    return 0;
+}

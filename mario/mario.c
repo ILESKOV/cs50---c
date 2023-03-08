610194f11b9144b8104c4ad2a09a765b493066e2
Ivan Leskov <ivan.leskov@protonmail.com>
2023-03-08 23:27:08 +0100

    add mario advanced

--- mario/mario.c
+++ mario/mario.c
@@ -17,12 +17,9 @@ int main() {
         for (int k = 1; k <= i; k++) {
             printf("#");
         }
-        printf("   ");
+        printf("  ");
         for (int k = 1; k <= i; k++) {
             printf("#");
-        }
-        for (int j = 1; j <= height - i; j++) {
-            printf(" ");
         }
         printf("\n");
     }
